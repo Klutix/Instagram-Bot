d86078ddf0c4ea09ea31eb9a1c6864d103dfe202
Gabriel Wohlford <GabrielWohlford@gmail.com>
2020-06-12 00:01:18 -0600

    Primary Configuration Changes
    -Add configuration through configuration parser
    -removed import to use old config
    -removed date from results and field
    -removed some dead comments
    -changed code that runs the program loop into a function called run
    -Added check and exit if user has not been set after first run (for google profile path)

--- instagram_bot.py
+++ instagram_bot.py
@@ -3,24 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-#--------------------------------------------------------------
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#---------------------------------------------------------------
+
 import sqlite3
-from CONFIG import * #user configuration file
+#from CONFIG import * #user configuration file***
 import configparser #using this for results.. may switch config to this
 
 import datetime
-import os
-import time
-
-
-
-
+import os       #used for working path
+import time     #used for sleep
+import sys      #used for command line arg
 
 class instagram_automation:
-    def __init__(self, profile_path = ''): #path is hardcoded for windows
+    def __init__(self): #path is hardcoded for windows
+        #first set up config
+        self.set_config()
         #Chrome Driver Setup---------------------------------------------
         #options create options to handle profiles and or headless
         self._chrome_options = Options()
@@ -31,16 +30,15 @@ class instagram_automation:
         #set the environment
         os.environ["webdriver.chrome.driver"] = self.chromedriver
 
-        #make headless
+        #make headless #might be flagging server.. need to test this
         #self._chrome_options.add_argument('--remote-debugging-port=9222')
-        #self._chrome_options.add_argument("--headless")
-        #self._chrome_options.add_argument("--disable-gpu") # needed for headless
+        #self._chrome_options.add_argument("--headless")        #self._chrome_options.add_argument("--disable-gpu") # needed for headless
         #self._chrome_options.add_argument("window-size=1200x600");
         #self._chrome_options.add_argument("--disable-extensions")
     
         #add the profile to options
         self._chrome_options.add_argument('--profile-directory=Default')
-        self._chrome_options.add_argument("user-data-dir={path}".format(path = profile_path))
+        self._chrome_options.add_argument("user-data-dir={path}".format(path = self.GOOGLE_PROFILE_PATH))
         
         #create the bot driver
         self.driver = webdriver.Chrome(self.chromedriver, options = self._chrome_options)
@@ -50,8 +48,10 @@ class instagram_automation:
             self.c = self.conn.cursor()
         except sqlite3.Error:
             print ("Error open db.\n")
+            print("Exiting Program")
+            input("Press Enter to Exit Program...")
             exit()
-
+            
         try:
             #set up two tables Questions and jobs applied to
             self.c.execute('''CREATE TABLE tblUrlsVisted 
@@ -73,74 +73,88 @@ class instagram_automation:
             
     def __del__(self):
         self.driver.close()
- 
+
+        
+    def set_config(self):
+        #create configuration file if not exist
+        if not os.path.exists('config.ini'):
+            print("Creating config file: Results.ini")
+            date = datetime.datetime.now()
+            config = configparser.ConfigParser()
+            config['CONFIG'] = {}
+            config['CONFIG']['GOOGLE_PROFILE_PATH'] = "C:\\Users\\YOUR_USER_NAME\\AppData\\Local\\Google\\Chrome\\User Data" 
+            config['CONFIG']['LIKE_LIMIT_PER_CATGEORY'] = '5'
+            config['CONFIG']['GRAB_LIMIT'] = '300'
+            config['CONFIG']['LIKE_DELAY_RANGE'] = '15 60'
+            config['CONFIG']['SCROLL_COUNT'] = '5'
+            config['CONFIG']['CATEGORIES'] = "#Fractals #FractalArt #prettyArt"
+            with open('CONFIG.ini', 'w') as configfile:
+                config.write(configfile)
+            print("Created CONFIG.ini")
+            print("!! -- Replace <YOUR_USER_NAME> in GOOGLE_PROFILE_PATH in CONFIG.ini then rerun program")
+            input("Press Enter to continue...")
+            exit()
+
+        #pull from the config file
+        config = configparser.ConfigParser()
+        config.read('CONFIG.ini')
+        
+        self.GOOGLE_PROFILE_PATH     = config['CONFIG']['GOOGLE_PROFILE_PATH']
+        self.LIKE_LIMIT_PER_CATGEORY = int(config['CONFIG']['LIKE_LIMIT_PER_CATGEORY'])
+        self.GRAB_LIMIT              = int(config['CONFIG']['GRAB_LIMIT'])
+        self.LIKE_DELAY_RANGE        = list(map(int,config['CONFIG']['LIKE_DELAY_RANGE'].split()))
+        self.SCROLL_COUNT            = int(config['CONFIG']['SCROLL_COUNT'])
+        self.CATEGORIES              = config['CONFIG']['CATEGORIES'].split()
+
+        #make sure user has been set correctly
+        if('YOUR_USER_NAME' in self.GOOGLE_PROFILE_PATH):
+             print("!! -- Replace <YOUR_USER_NAME> in GOOGLE_PROFILE_PATH in CONFIG.ini then rerun program")
+             print("Exiting Program")
+             input("Press Enter to Exit Program...")
+             exit()
 
     def create_results_file(self):
-        print("Creating results file: Results.ini")
+        print("Creating results file: Results.txt")
         date = datetime.datetime.now()
         config = configparser.ConfigParser()
         config['Results_{}'.format(date)] = {}
-        config['Results_{}'.format(date)]['DATE'] = str(date)
         config['Results_{}'.format(date)]['LIKES'] = str(self.likes)
         config['Results_{}'.format(date)]['SKIPS'] = str(self.skips)
         config['Results_{}'.format(date)]['GRABED'] = str(self.grabbed)
-        #config['Results_{}'.format(date]['LTD'] = results[4]
-        with open('Results.ini', 'a') as configfile:
+
+        with open('Results.txt', 'a') as configfile:
             config.write(configfile)
         print("Created results file.")
+        
     def sleep(self,_from,_to):
         sleep_time = randint(_from,_to)
         print('sleeping for {} seconds'.format(sleep_time)) 
         time.sleep(sleep_time)
-        
-        
+                
     def open_instagram(self):
-        #open instagram
         print('opening instagram')
         self.driver.get('https://www.instagram.com/')
-        
-#this function is abit depreciated and profile is better to work with        
-##    def login(self,username, password):
-##        #get username textbox
-##        elem = self.driver.find_element_by_name("username")
-##        elem.clear()
-##        
-##        #send username to textbox
-##        elem.send_keys(username)
-##
-##        #get password textbox
-##        elem = self.driver.find_element_by_name("password")
-##        elem.clear()
-##        
-##        #send password to textbox
-##        elem.send_keys(password)
-##        
-##        #delay so login button can appear
-##        time.sleep(2)
-##        
-##        #attempt to login
-##        print("attempting login credentials")
-##        elem.send_keys(Keys.ENTER)
 
     def search(self,search_text):
         #find search textbar
-       
-        elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Search"]')))
-
-        #olf version without wait
-        #elem = self.driver.find_element_by_xpath('//input[@placeholder="Search"]')
-        elem.clear()
+        try:
+            elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Search"]')))
+        except NoSuchElementException:
+            print("Web page either failed to load correctly or You not have Instgagram Credentials saved to your Chrome profile")
+            print("--Make sure you have set <YOUR_USER_NAME> in GOOGLE_PROFILE_PATH in CONFIG.ini.")
+            print("Exiting Program")
+            input("Press Enter to Exit Program...")
+            exit()
         
         #send search_text to textbox
         elem.send_keys(search_text)
         print('searching for {}'.format(search_text))
         #sleep so drop down has a moment to populate
-        time.sleep(2) #NEED THIS
+        time.sleep(2) #DO NOT DELETE
     
         #get first result
         #In dropdown list go DOWN one then press ENTER
         elem.send_keys(Keys.DOWN)
-        #time.sleep(1) # do i need this?
         elem.send_keys(Keys.ENTER)
         time.sleep(3) #let things load
 
@@ -164,7 +178,7 @@ class instagram_automation:
             if(count == limit):
                 break
             url = elem.get_attribute("href")
-            #grab on post with /p which your user post
+            #grab on post with /p which means its a user post
             if('.com/p' in url):
                 #check if url is already exist in DB
                 if(self._is_used_url(url)):
@@ -187,13 +201,8 @@ class instagram_automation:
             #navigate to url in url list
             self.driver.get(post)
             
-            #wait a moment for page to load
-            #time.sleep(2)
-            
             #get the like icon
             elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR , 'svg')))
-            #old vewrsion without wait
-            #elem = self.driver.find_element_by_css_selector('svg')
 
             sql = ''' INSERT INTO tblUrlsVisted(url) VALUES(?) '''
             
@@ -205,35 +214,35 @@ class instagram_automation:
                 self.c.execute(sql,[post])
                 self.conn.commit()
                 self.ltd = self.ltd + 1     #likes to date
-                self.likes= self.likes + 1  #likes this session
+                self.likes = self.likes + 1  #likes this session
             elif(elem.get_attribute("aria-label") == "Unlike"): 
                 #we have already liked this in the past sorted added to the DB
                 self.c.execute(sql,[post])
                 self.conn.commit()
                 self.ltd = self.ltd + 1     #likes to date
                 self.skips = self.skips + 1
+                print('Skipping {}'.format(post))
             else:
                 print("Something isnt right.. not loaded or not were im supposed to be. Moving on..")
                 continue
             #sleep for moment to keep bot human like    
             self.sleep(sleep_from,sleep_to)
-        
 
-insta_bot = instagram_automation(GOOGLE_PROFILE_PATH)
-insta_bot.open_instagram()
-#give browser a few seconds to main page
-#time.sleep(6)
-for c in CATEGORIES:   
-    insta_bot.search(c)
-    #give browser a few seconds to load search results
-    #scroll down to get more results
-    for x in range(0, SCROLL_COUNT):
-        insta_bot.scroll()
-        time.sleep(1)
-    #get all urls_post on page
-    urls = insta_bot.get_posts_urls(GRAB_LIMIT)
-    insta_bot.like_posts(urls,LIKE_LIMIT_PER_CATGEORY,LIKE_DELAY_RANGE[0],LIKE_DELAY_RANGE[1])
-    insta_bot.create_results_file()
-print("Ending Driver Like :)")
+    def run(self):
+        self.open_instagram()
+        for c in self.CATEGORIES:   
+            self.search(c)
+            #scroll down to get more results
+            for x in range(0, self.SCROLL_COUNT):
+                self.scroll()
+                time.sleep(1)
+            #get all urls_post on page
+            urls = self.get_posts_urls(self.GRAB_LIMIT)
+            self.like_posts(urls,self.LIKE_LIMIT_PER_CATGEORY,self.LIKE_DELAY_RANGE[0],self.LIKE_DELAY_RANGE[1])
+            self.create_results_file()
+        print("Finished Running")
+
+insta_bot = instagram_automation()
+insta_bot.run()
 del insta_bot
 
