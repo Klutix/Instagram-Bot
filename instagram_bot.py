aaeb66efd89577e693e90741779f582e10ef033d
Gabriel Wohlford <GabrielWohlford@gmail.com>
2020-06-15 00:21:56 -0600

    Major Update - Added Terminal layout, and lots of little changes
    Added Terminal Layout
    -added numbers to option
    -added colors to options
    
    Added Query to check total post in day
    Added more out variables/
    Added various checks that otherwise caused errors.
    
    Looking good. Need to move bottom logic into class then refactor/clean

--- instagram_bot.py
+++ instagram_bot.py
@@ -3,21 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import sqlite3
 import threading
-
 import configparser 
-
 import datetime
 import os       #used for working path
 import time     #used for sleep
 import sys      #used for command line arg
 
 class instagram_automation:
+    
     def __init__(self): #path is hardcoded for windows
         #first set up config
         self.create_example_ini()
@@ -55,6 +52,18 @@ class instagram_automation:
         self._paused = False
 
         self._state = "OFF"
+
+        #more out variables
+        self._urls_remaining_count = 0
+        self._urls_in_queue = 0
+        self._time_remaining = 0
+        self._last_url = ''
+        self._category_current = ''
+        self._date = datetime.date.today()
+        self._likes_today = 0
+
+        
+            
         
    
         
@@ -62,19 +71,19 @@ class instagram_automation:
     def get_GOOGLE_PROFILE_PATH():
         return _GOOGLE_PROFILE_PATH
     def set_GOOGLE_PROFILE_PATH(GOOGLE_PROFILE_PATH):
-        _GOOGLE_PROFILE_PATH = GOOGLE_PROFILE_PATH
+        self._GOOGLE_PROFILE_PATH = GOOGLE_PROFILE_PATH
     def get_LIKE_LIMIT_PER_CATGEORY():
-        return _LIKE_LIMIT_PER_CATGEORY
+        return self._LIKE_LIMIT_PER_CATGEORY
     def set_LIKE_LIMIT_PER_CATGEORY(LIKE_LIMIT_PER_CATGEORY):
-        _LIKE_LIMIT_PER_CATGEORY
+        self._LIKE_LIMIT_PER_CATGEORY = LIKE_LIMIT_PER_CATGEORY
     def get_LIKE_DELAY_RANGE():
-        return _LIKE_DELAY_RANGE
+        return self._LIKE_DELAY_RANGE
     def set_LIKE_DELAY_RANGE(LIKE_DELAY_RANGE):
-        _LIKE_DELAY_RANGE = LIKE_DELAY_RANGE
+        self._LIKE_DELAY_RANGE = LIKE_DELAY_RANGE
     def get_CATEGORIES():
-        return _CATEGORIES
+        return self._CATEGORIES
     def set_CATEGORIES(CATEGORIES):
-        _CATEGORIES = CATEGORIES
+        self._CATEGORIES = CATEGORIES
 
     def get_state(self):
         return (self._state)
@@ -88,14 +97,14 @@ class instagram_automation:
     def create_example_ini(self):
          if not os.path.exists('config.ini'):
             print("Creating config file: Results.ini")
-            date = datetime.datetime.now()
-            config = configparser.ConfigParser()
+            #config = configparser.ConfigParser()
             config['CONFIG'] = {}
             config['CONFIG']['GOOGLE_PROFILE_PATH'] = "C:\\Users\\YOUR_USER_NAME\\AppData\\Local\\Google\\Chrome\\User Data" 
             config['CONFIG']['LIKE_LIMIT_PER_CATGEORY'] = '5'
             config['CONFIG']['LIKE_DELAY_RANGE'] = '15 60'
             config['CONFIG']['SCROLL_COUNT'] = '5'
             config['CONFIG']['CATEGORIES'] = "#Fractals #FractalArt #prettyArt"
+            
             with open('CONFIG.ini', 'w') as configfile:
                 config.write(configfile)
             print("Created CONFIG.ini")
@@ -134,10 +143,15 @@ class instagram_automation:
         config['Results_{}'.format(date)] = {}
         config['Results_{}'.format(date)]['LIKES'] = str(self._likes)
         config['Results_{}'.format(date)]['SKIPS'] = str(self._skips)
+        config['Results_{}'.format(date)]['LIKES_TODAY'] = str(self._likes_today)
 
         with open('Results.txt', 'a') as configfile:
             config.write(configfile)
+
         print("Created results file.")
+
+    def update_likes_today():
+        pass
 
     #custom sleep function to handle in possible range
     def sleep(self,_from,_to):
@@ -207,14 +221,22 @@ class instagram_automation:
                 else:
                     url_list.append(url)
                     print(url)
-        #count add to total grabbed            
+        #count add to total grabbed
+        self._urls_remaining_count = len(url_list)
+        a = self._LIKE_LIMIT_PER_CATGEORY
+        if(self._LIKE_LIMIT_PER_CATGEORY <= self._urls_remaining_count):
+            self._urls_in_queue = self._LIKE_LIMIT_PER_CATGEORY
+        else:
+            self._urls_in_queue = a
         return url_list
 
     #likes all post urls in a url list and uses range of manage like speed
     def like_posts(self,post_list_urls):
         count = 0
         for post in post_list_urls:
+            self._last_url = post
             state = self._manage_state()
+            self._print_feedback()
             if(state == "OFF"):
                 break
             if (count == self._LIKE_LIMIT_PER_CATGEORY):
@@ -227,20 +249,20 @@ class instagram_automation:
             #get the like icon
             elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR , 'svg')))
 
-            sql = ''' INSERT INTO tblUrlsVisted(url) VALUES(?) '''
+            sql = ''' INSERT INTO tblUrlsVisted(url,date) VALUES(?,?) '''
             
             #check if shows "Like or Unlike" if like exist on page then go and like
             if (elem.get_attribute("aria-label") == "Like"):
                 elem.click()
                 count = count + 1
                 print('{i} liked post {p}'.format(p = post, i = self._likes + 1))         
-                self.c.execute(sql,[post])
+                self.c.execute(sql,[post,self._date])
                 self.conn.commit()
                 #self.ltd = self.ltd + 1      #likes to date
                 self._likes = self._likes + 1  #likes this session
             elif(elem.get_attribute("aria-label") == "Unlike"): 
                 #we have already liked this in the past sorted added to the DB
-                self.c.execute(sql,[post])
+                self.c.execute(sql,[post,self._date])
                 self.conn.commit()
                 #self.ltd = self.ltd + 1     #likes to date
                 self._skips = self._skips + 1
@@ -248,8 +270,11 @@ class instagram_automation:
             else:
                 print("Something isnt right.. not loaded or not were im supposed to be. Moving on..")
                 continue
+
+            self._urls_remaining_count = self._urls_remaining_count - 1
+            self._urls_in_queue = self._urls_in_queue - 1
             #sleep for moment to keep bot human like    
-            self.sleep(self._LIKE_DELAY_RANGE[0],self._LIKE_DELAY_RANGE[1])
+            self._sleep_with_iterupt(self._LIKE_DELAY_RANGE[0],self._LIKE_DELAY_RANGE[1])
 
     #main bot driver
     def run(self):
@@ -279,13 +304,16 @@ class instagram_automation:
         
         self.open_instagram()
         for c in self._CATEGORIES:
+            self._category_current = c
             state = self._manage_state()
+            self._print_feedback()
             if(state == "OFF"):
                 break
             print("Searching..")
             self.search(c)
             #scroll down to get more results
             for x in range(0, self._SCROLL_COUNT):
+                self._manage_state()
                 self.scroll()
                 time.sleep(1)
             #get all urls_post on page
@@ -297,16 +325,25 @@ class instagram_automation:
 
     def _manage_state(self):
         if(not self._enabled):
-            print("break")
-            self._state == "OFF"
-            return self._state
+            self._state = "OFF"
+            self._print_feedback()
+        
+            #exit()
         if(self._paused):
                 print("PAUSED")
+        
         while(self._paused):
             self._state ="PAUSED"
+            self._print_feedback()
             time.sleep(1)
-            self._state = "ON"
+        self._state = "ON"
         return self._state
+
+    def count_todays_likes(self):
+        sql = "SELECT COUNT(url) from tblUrlsVisted WHERE date = '{}'".format(self._date)
+        self.c.execute(sql)
+        self._likes_today = self.c.fetchone()[0]
+        
         
     def pause(self, v):
         self._paused = v
@@ -314,10 +351,70 @@ class instagram_automation:
     def enabled(self,v):
         self._enabled = v
 
+    # define our clear function 
+    def _clear(self): 
+        # for windows 
+        if os.name == 'nt': 
+            _ = os.system('cls') 
+      
+        # for mac and linux(here, os.name is 'posix') 
+        else: 
+            _ = os.system('clear')
 
+    def _sleep_with_iterupt(self,_from,_to):
+        sleep_time = randint(_from,_to)
+        self._time_remaining = sleep_time
+        for t in range(0, sleep_time):
+            if(not self._enabled):
+                self.create_results_file()
+                input("Bot Was STOPPED.. Press Enter to Exit Program")
+                exit()
+            time.sleep(1)
+            self._time_remaining =  self._time_remaining - 1
+            state = self._manage_state()
+            self._print_feedback()
+            
+
+    def _print_feedback(self):
         
+        
+        self.count_todays_likes()
+        out_layout = str("\033[1;33;40m-----------------------Instagram Automation Tool------------------------\n"
+            "  \033[1;35;40mUrls Available:\033[0;33;40m{urls_remaining_count}           \033[92m STATUS:{state}\n" #green
+            "  \033[1;35;40mUrls in Queue:\033[0;33;40m{queue}                    \033[94m Sleep time Remaining:\033[1;37;40m{tm}\n" 
+            "  \033[1;35;40mLimit:\033[0;33;40m{limit}"
+            "\n\033[0;33;40m"
+            " * \033[1;36;40mLast Url:\033[0;33;40m{last_url}\n"
+            " * \033[1;36;40mCatgeorgies:\033[0;33;40m{catgories_list}\n"                       
+            " * \033[1;36;40mCurent Category: \033[1;34;40m{current_category}\n"
+            "\n"                                                                                            
+            " \033[0;33;40m-\033[1;37;40m Likes:\033[1;34;40m{likes}\n"
+            " \033[0;33;40m-\033[1;37;40m Skips:\033[1;34;40m{skips}\n"
+            " \033[0;33;40m#\033[1;37;40m Total likes Today :\033[1;34;40m{today_likes}\n"
+            "\033[1;33;40m************************************************************************\n"
+            "------------------------------------------------------------------------\n"
+            " \033[1;34;40mCOMMANDS LIST:\n"                               
+            "   \033[1;33;40m1)PAUSE\n" 
+            "   2)STOP\n"
+            "   3)RESUME\n"
+            "\n"             
+            "\033[1;37;40mEnter Command or # at anytime...\n")
+        self._clear()
+        print(out_layout.format(urls_remaining_count = self._urls_remaining_count,
+                                state = self._state,
+                                tm = self._time_remaining,
+                                last_url = self._last_url,
+                                queue = self._urls_in_queue,
+                                catgories_list = self._CATEGORIES,
+                                current_category = self._category_current,
+                                likes = self._likes,
+                                skips = self._skips,
+                                today_likes = self._likes_today,
+                                limit = self._LIKE_LIMIT_PER_CATGEORY))
+
+    
 #int main---------------------------------------------still testing class above
-        #with threading functionality
+#with threading functionality
 i = instagram_automation()
 t = threading.Thread(target=i.run)
 t.start()
@@ -325,18 +422,18 @@ t.start()
 def cmd_in():
     while(True):
         state = i.get_state()
-        print("Enter Commands at any time\n")
-        r = input("!!-----------COMMANDS-------------\n\n-----PAUSE\n-----RESUME\n-----STOP\n-----EXIT\n\n")
+        r = input()
         r = r.lower()
-        if(r == "stop"):
+        if(r == "stop" or r == "2"):
             i.enabled(False)
-            if(i.get_state() == "OFF" or i.get_state()  == "DONE"):
+            if(i.get_state() == "OFF" or i.get_state()  == "DONE"): #hmm quarky..maybe bad
                 return
-        elif(r == "pause"):
+            return
+        elif(r == "pause" or r == "1"):
             i.pause(True)
             if(i.get_state() == "PAUSED"):
                 continue
-        elif(r == "resume"):
+        elif(r == "resume"or r == "3"):
             i.pause(False)
             if(i.get_state() == "ON"):
                 continue
@@ -348,7 +445,6 @@ def cmd_in():
 if __name__ == "__main__":    
     cmd_in()
     del i
-    input("Program finished press enter to exit...")
     exit()
         
 
